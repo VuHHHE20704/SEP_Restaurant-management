67649816a63e8f0e2d23e3516a990917abaea98f
VuHHHE180800 <vuhhhe180800@fpt.edu.vn>
2026-02-09 02:03:13 +0700

    config program.cs

--- SEP_Restaurant management/Program.cs
+++ SEP_Restaurant management/Program.cs
@@ -1,19 +1,24 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using SEP_Restaurant_management.Models;
+using SEP_Restaurant_management.ProgramConfig;
 
 var builder = WebApplication.CreateBuilder(args);
-
+builder.Services.AddDbContext<SepDatabaseContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("MyCnn"));
+});
+builder.Services.AddMyServices1();
+builder.Services.AddMyServices2();
+builder.Services.AddMyServices3();
+builder.Services.AddMyServices4();
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<SepDatabaseContext>(options =>
-{
-    options.UseSqlServer(builder.Configuration.GetConnectionString("MyCnn"));
-});
+
 
 // Add Identity
 builder.Services.AddIdentity<UserIdentity, IdentityRole>()
